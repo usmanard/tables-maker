34968c2afb905776e4ca36dea5750b0930ccfc45
Usman Arshad <us.arshad@gmail.com>
2024-05-17 00:42:16 +0500

    Tables Maker Created using Javascript

---
+++ app/app.js
@@ -0,0 +1,29 @@
+let theNumber = document.getElementById("table");
+let tableTitle = document.getElementById("tableTitle");
+let tableBody = document.getElementById("tableBody");
+function tableGenerator() {
+  reset();
+  if (theNumber.value == "") {
+    tableTitle.innerText = "Please enter a valid number";
+    tableBody.innerText = "";
+  } else {
+    for (let counter = 1; counter <= 10; counter++) {
+      // console.log(
+      //   theNumber.value + " * " + counter + " = " + theNumber.value * counter
+      // );
+
+      tableTitle.innerText = "Table of " + theNumber.value;
+      tableBody.innerText +=
+        theNumber.value +
+        " * " +
+        counter +
+        " = " +
+        theNumber.value * counter +
+        "\n";
+    }
+  }
+}
+function reset() {
+  tableTitle.innerText = "";
+  tableBody.innerText = "";
+}
