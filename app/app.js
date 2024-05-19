9b11330a0fa727847c2f4e5ae8dd71a2eea0670b
Usman Arshad <usman@Usmans-MacBook-Pro.local>
2024-05-19 21:20:04 +0500

    Added Second parameter for multiplier

--- app/app.js
+++ app/app.js
@@ -1,17 +1,21 @@
-let theNumber = document.getElementById("table");
+let theNumber = document.getElementById("form-table");
+
+let theMultiplier = document.getElementById("form-multiplier");
+
 let tableTitle = document.getElementById("tableTitle");
 let tableBody = document.getElementById("tableBody");
+let btn = document.getElementById("btn");
+
 function tableGenerator() {
   reset();
-  if (theNumber.value == "") {
+  if (theNumber.value == "" && theMultiplier.value == "") {
     tableTitle.innerText = "Please enter a valid number";
-    tableBody.innerText = "";
+    tableBody.style.display = "none";
   } else {
-    for (let counter = 1; counter <= 20; counter++) {
+    for (let counter = 1; counter <= theMultiplier.value; counter++) {
       // console.log(
       //   theNumber.value + " * " + counter + " = " + theNumber.value * counter
       // );
-
       tableTitle.innerText = "Table of " + theNumber.value;
       tableBody.innerText +=
         theNumber.value +
